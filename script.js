ba4e581442e05dc929bc30c622c1e998dcbc7d44
devgabrielbarbosa <lemosbgabriel@gmail.com>
2023-05-07 18:00:30 -0300

    modificação na font-family

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Menu Hamburguer
+
 const btnMobile = document.getElementById("btn-mobile");
 
 function toggleMenu() {
@@ -7,6 +9,49 @@ function toggleMenu() {
 
 btnMobile.addEventListener("click", toggleMenu);
 
+
+// Slides
+
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
+
+let currentSlide = 0;
+
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
+}
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1;
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
+
+
+
+//frutos do cerrado
 
 const frutas = [
     {
@@ -266,39 +311,4 @@ function frutasCerrado() {
 
 frutasCerrado();
 
-const slider = document.querySelectorAll('.slider');
-const btnPrev = document.getElementById('prev-button');
-const btnNext = document.getElementById('next-button');
 
-let currentSlide = 0;
-
-function hideSlider() {
-  slider.forEach(item => item.classList.remove('on'))
-}
-
-function showSlider() {
-  slider[currentSlide].classList.add('on')
-}
-
-function nextSlider() {
-  hideSlider()
-  if(currentSlide === slider.length -1) {
-    currentSlide = 0
-  } else {
-    currentSlide++
-  }
-  showSlider()
-}
-
-function prevSlider() {
-  hideSlider()
-  if(currentSlide === 0) {
-    currentSlide = slider.length -1
-  } else {
-    currentSlide--
-  }
-  showSlider()
-}
-
-btnNext.addEventListener('click', nextSlider)
-btnPrev.addEventListener('click', prevSlider)
